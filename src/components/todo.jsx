8d905fbdfd8621834a7cee31b01a2a063d9046de
muhamadjaya <ahmadzainsiji@gmail.com>
2022-03-21 16:49:34 +0700

    change class component to functional component

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,33 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Todo extends Component {
-  render() {
-    return (
-      <>
-        <td style={{ width: 15 }} className="text-center">
-          <input
-            type="checkbox"
-            defaultChecked={this.props.todo.completed}
-            onChange={() => this.props.fooDoneDone(this.props.todo)}
-          />
-        </td>
-        <td>{this.renderTodo()}</td>
-        <td style={{ width: 100 }} className="text-center">
-          <button
-            onClick={() => this.props.fooDelete(this.props.todo.id)}
-            className="btn btn-danger btn-sm"
-          >
-            Delete
-          </button>
-        </td>
-      </>
-    );
-  }
+const Todo = ({ todo, fooDelete, fooDoneDone }) => {
+  const checkTodo = () => {
+    if (todo.completed) return <s>{todo.value}</s>;
+    else return todo.value;
+  };
 
-  renderTodo() {
-    if (this.props.todo.completed) return <s>{this.props.todo.value}</s>;
-    else return this.props.todo.value;
-  }
-}
+  return (
+    <>
+      <td style={{ width: 15 }} className="text-center">
+        <input
+          type="checkbox"
+          defaultChecked={todo.completed}
+          onChange={() => fooDoneDone(todo)}
+        />
+      </td>
+      <td>{checkTodo()}</td>
+      <td style={{ width: 100 }} className="text-center">
+        <button
+          onClick={() => fooDelete(todo.id)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      </td>
+    </>
+  );
+};
 
 export default Todo;
